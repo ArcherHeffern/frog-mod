045ac7ae2d188fc14d671d4a2d9ad8fb5c2f2aa7
ArcherHeffern <heffernarcher@gmail.com>
2025-07-05 19:59:18 -0400

    Add frog legs.

--- src/main/java/archer/frog/ModItems.java
+++ src/main/java/archer/frog/ModItems.java
@@ -4,9 +4,14 @@ import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
 import net.fabricmc.fabric.api.registry.FuelRegistryEvents;
+import net.minecraft.component.type.ConsumableComponents;
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.consume.ApplyEffectsConsumeEffect;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
@@ -19,6 +24,7 @@ import java.util.function.Function;
 public class ModItems {
 
     public static final Item FROG_ESSENCE = register("frog_essence", Item::new, new Item.Settings());
+    public static final Item FROG_LEGS = register("frog_legs", Item::new, frogLegSettings());
 
     public static final RegistryKey<ItemGroup> CUSTOM_ITEM_GROUP_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(FrogModInitializer.MOD_ID, "item_group"));
     public static final ItemGroup CUSTOM_ITEM_GROUP = FabricItemGroup.builder()
@@ -29,6 +35,8 @@ public class ModItems {
     public static void initialize() {
         initializeItemGroup();
         initializeFrogEssence();
+        initializeFrogLegs();
+        new Item.Settings().food(new FoodComponent.Builder().alwaysEdible().build());
     }
 
     private static void initializeItemGroup() {
@@ -37,6 +45,7 @@ public class ModItems {
 
         ItemGroupEvents.modifyEntriesEvent(CUSTOM_ITEM_GROUP_KEY).register(itemGroup -> {
             itemGroup.add(ModItems.FROG_ESSENCE);
+            itemGroup.add(ModItems.FROG_LEGS);
             // ...
         });
     }
@@ -46,10 +55,21 @@ public class ModItems {
         // .register((itemGroup) -> itemGroup.add(ModItems.FROG_ESSENCE));
         CompostingChanceRegistry.INSTANCE.add(ModItems.FROG_ESSENCE, 0.3f);
         // TODO: Figure out how to look at the FuelRegistryEvents.BuildCallback Interface???
-        FuelRegistryEvents.BUILD.register((builder, context) -> {
-            builder.add(ModItems.FROG_ESSENCE, 30 * 20);
-        });
+        FuelRegistryEvents.BUILD.register((builder, context) -> builder.add(ModItems.FROG_ESSENCE, 30 * 20));
+    }
 
+    private static Item.Settings frogLegSettings() {
+        int nutrition = 2;
+        float hungerProbability = 0.5f;
+        int hungerDurationTicks = 10 * 20;
+
+        return new Item.Settings()
+            .food(
+                    new FoodComponent.Builder().nutrition(nutrition).build(),
+                    ConsumableComponents.food().consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.HUNGER, hungerDurationTicks), hungerProbability)).build()
+            );
+    }
+    private static void initializeFrogLegs() {
     }
 
     private static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
